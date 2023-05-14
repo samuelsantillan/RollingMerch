847da7152fd8b5dfebd9ee391b12105b0659d18c
Mariana_Cervantes <cervantes.mariana.6017@gmail.com>
2023-05-13 22:01:54 -0300

    agrego funcionalidades a formulario login y registro

---
+++ home.js
@@ -0,0 +1,29 @@
+const user = JSON.parse(localStorage.getItem('login_exitoso')) || false
+if(!user){
+    window.location.href = 'index.html'
+}
+
+const logout = document.querySelector('#logout')
+
+logout.addEventListener('click', ()=>{
+    Swal.fire({
+        icon: 'warning',
+        title: 'Cerrar Sesión?',
+        showCancelButton: true,
+        confirmButtonText: 'Aceptar', 
+        cancelButtonText: 'Cancelar',
+        confirmButtonColor: '#C10001',
+        cancelButtonColor: '#9A9693',
+        background: '#31302F',
+        color: 'white',
+        backdrop: `rgba(0,0,14,0.4)` 
+      }).then((result) => {        
+        if (result.isConfirmed) {
+            localStorage.removeItem('login_exitoso')
+            window.location.href = 'index.html'
+        } else if (result.isDenied) {
+          return
+        }
+      })
+});
+
---
+++ registro.js
@@ -0,0 +1,38 @@
+const registerForm = document.querySelector('#registerForm')
+
+registerForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const name = document.querySelector('#name').value
+    const lastname = document.querySelector('#lastname').value
+    const email = document.querySelector('#email').value
+    const password = document.querySelector('#password').value
+
+    const Users = JSON.parse(localStorage.getItem('users')) || []
+    const usuarioRegistrado = Users.find(user => user.email === email)
+    if(usuarioRegistrado){
+        registerForm.reset();
+        return Swal.fire({
+            icon: 'error',
+            title: 'El usuario ya está registrado.',
+            showConfirmButton: true,  
+            confirmButtonColor: '#C10001',          
+            background: '#31302F',
+            color: 'white',
+            backdrop: `rgba(0,0,14,0.4)`           
+          })         
+    }else{
+        Users.push({name: name,lastname: lastname, email: email, password: password})
+        localStorage.setItem('users', JSON.stringify(Users))        
+        Swal.fire({
+            icon: 'success',
+            title: 'Registro exitoso',
+            confirmButtonColor: '#C10001'           
+          })
+        setTimeout(function(){
+            window.location.href = 'index.html';
+        }, 1500);
+    }
+})
+
+
+    
